ae4749415802304256aaaf375cf5f8052071f19d
QuantamWorld562-tech <quantam.world562@gmail.com>
2026-09-26 13:09:10 +0530

    update roadmapRoutes

--- server/src/routes/roadmapRoutes.ts
+++ server/src/routes/roadmapRoutes.ts
@@ -1,10 +1,13 @@
-import express from "express";
-import { generateRoadmap, getMyRoadmap } from "../controllers/roadmapController.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js"
+import { Router } from "express";
+import {
+  generateRoadmap,
+  getMyRoadmap,
+} from "../controllers/roadmapController.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
-const roadmapRouter = express.Router();
+const roadmapRouter: Router = Router();
 
-roadmapRouter.get("/my-roadmap", isAuthenticated,getMyRoadmap);
-roadmapRouter.post('/generate',isAuthenticated,generateRoadmap);
+roadmapRouter.get("/my-roadmap", isAuthenticated, getMyRoadmap);
+roadmapRouter.post("/generate", isAuthenticated, generateRoadmap);
 
 export default roadmapRouter;
